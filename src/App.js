96ae38b58987d4b862821a90767d2655b5d5c50e
Kartik Sihotiya <ksihotiya@gmail.com>
2021-02-15 13:03:02 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
 
 function App() {
+  
   return (
-    <div className="App">
-      
+    <div className="container">
+     <h2 className='hvr-pulse'>Remember that the happiest people are not those getting more, but those giving more.</h2>
     </div>
   );
 }
